46983cf216effc3798d0cc10ff5ab8c3b1908146
rizzacarla <rizzacarla.12@gmail.com>
2021-12-05 16:27:42 -0800

    setup front and backend

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+// const errorMiddleware = require("error-middleware")
+
+const app = express();
+const router = express.Router();
+
+// env variables
+const db = require("./config/keys").mongoURI; 
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Connected to MongoDB successfully!!"))
+.catch((err) => console.log(err))
+;
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+// app.use(require('../route/auth-router'));
+
+// error middleware
+// app.use(errorMiddleware);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
